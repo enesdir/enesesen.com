46ee8a43aaaa8d867a8c22b1b84a2a3e350bd4a5
Enes Esen <enesen@gmx.com>
2022-07-30 15:49:42 +0300

    fix: update packages

--- next.config.js
+++ next.config.js
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+/** @type {import('next').NextConfig} */
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 });
@@ -5,10 +7,9 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 const nextConfig = {
   eslint: {
     dirs: ['src'],
-  },
-  images: {
-    deviceSizes: [320, 480, 640, 750, 828, 960, 1080, 1200, 1440, 1920, 2048, 2560, 3840],
-    domains: ['res.cloudinary.com'],
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
   },
   typescript: {
     // !! WARN !!
@@ -17,10 +18,9 @@ const nextConfig = {
     // !! WARN !!
     ignoreBuildErrors: true,
   },
-  eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
-    ignoreDuringBuilds: true,
+  images: {
+    deviceSizes: [320, 480, 640, 750, 828, 960, 1080, 1200, 1440, 1920, 2048, 2560, 3840],
+    domains: ['res.cloudinary.com'],
   },
 };
 
