9baf474f9af7434e05dfd99f0e7c9e4b0e35a23b
Enes Esen <enesen@gmx.com>
2022-10-04 00:37:24 +0300

    feat: project detail added

--- src/features/ProjectSection/types/ProjectType.ts
+++ src/features/ProjectSection/types/ProjectType.ts
@@ -1,6 +1,9 @@
 export type ProjectType = {
   title: string;
+  shortDescription: string;
   description: string;
+  slug: string;
+  position: 'Full Stack Developer' | 'Front-End Developer' | 'Backend Developer';
   technologies: string[];
   projectDate: string;
   projectDuration: string;
