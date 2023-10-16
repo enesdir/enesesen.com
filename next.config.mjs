bcee973de912577302d4c090079b7b7e88e274f4
Enes Esen <enesen@gmx.com>
2023-10-16 16:16:22 +0300

    fix: update packages & build error

--- next.config.mjs
+++ next.config.mjs
@@ -10,6 +10,73 @@ const withBundleAnalyzer = configureBundleAnalyzer({
 	enabled: process.env.ANALYZE === 'true',
 })
 
+// TODO: find better rule for script-src
+// https://securityheaders.com
+const ContentSecurityPolicy = `
+  default-src 'self';
+  base-uri 'self';
+  font-src 'self' https: data:;
+  form-action 'self';
+  frame-ancestors 'self';
+  frame-src 'self' calendly.com;
+  manifest-src 'self';
+  object-src 'none';
+  script-src 'self' 'unsafe-eval' 'unsafe-inline';
+  style-src 'self' https: 'unsafe-inline';
+  img-src * blob: data:;
+  connect-src *;
+  worker-src 'self' blob:;
+  upgrade-insecure-requests
+`
+const securityHeaders = [
+	// https://developer.mozilla.org/en-US/docs/Web/HTTP/CSP
+	{
+		key: 'Content-Security-Policy',
+		value: ContentSecurityPolicy.replace(/\s{2,}/g, ' ').trim(),
+	},
+	// https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Cross-Origin-Opener-Policy
+	{
+		key: 'Cross-Origin-Opener-Policy',
+		value: 'same-origin',
+	},
+	// https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Cross-Origin-Resource-Policy
+	{
+		key: 'Cross-Origin-Resource-Policy',
+		value: 'same-origin',
+	},
+	// https://web.dev/origin-agent-cluster/
+	{
+		key: 'Origin-Agent-Cluster',
+		value: '?1',
+	},
+	// https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Referrer-Policy
+	{
+		key: 'Referrer-Policy',
+		value: 'strict-origin-when-cross-origin',
+	},
+	// https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-Content-Type-Options
+	{
+		key: 'X-Content-Type-Options',
+		value: 'nosniff',
+	},
+	// https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-Frame-Options
+	{
+		key: 'X-Frame-Options',
+		value: 'SAMEORIGIN',
+	},
+	// https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-DNS-Prefetch-Control
+	{
+		key: 'X-DNS-Prefetch-Control',
+		value: 'on',
+	},
+	// https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Feature-Policy
+	// Opt-out of Google FLoC: https://amifloced.org/
+	{
+		key: 'Permissions-Policy',
+		value: 'self',
+	},
+]
+
 /** @type {import('next').NextConfig} nextConfig */
 const nextConfig = {
 	eslint: {
@@ -25,17 +92,40 @@ const nextConfig = {
 		ignoreBuildErrors: true,
 	},
 	images: {
+		remotePatterns: [
+			{
+				protocol: 'https',
+				hostname: 'res.cloudinary.com',
+			},
+		],
 		deviceSizes: [320, 480, 640, 750, 828, 960, 1080, 1200, 1440, 1920, 2048, 2560, 3840],
 		domains: ['res.cloudinary.com'],
+		contentDispositionType: 'attachment',
+		contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
 	},
 	swcMinify: true,
 	reactStrictMode: true,
 	poweredByHeader: false,
 	experimental: {
-		appDir: true,
+		typedRoutes: true,
+		serverActions: true,
+		outputFileTracingExcludes: {
+			'/*': ['./public/favicon/**/*.png'],
+			'/**/*': ['./public/favicon/**/*.png'],
+			'*': [
+				'node_modules/@swc/core-linux-x64-gnu',
+				'node_modules/@swc/core-linux-x64-musl',
+				'node_modules/@esbuild/linux-x64',
+			],
+		},
 	},
+
 	async headers() {
 		return [
+			{
+				source: '/(.*)',
+				headers: securityHeaders,
+			},
 			{
 				source: '/favicon/:all*',
 				headers: [
